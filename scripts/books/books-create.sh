b4cf6d723ac353ed0f42c4d72ba5bfae8e01d771
PatODay <poday22@gmail.com>
2018-09-05 16:27:03 -0400

    Created & successfully tested curl scripts
    Able to perform all CRUD actions via curl on `books` resource

---
+++ scripts/books/books-create.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl --include --request POST 'http://localhost:3000/books' \
+--header "Content-type: application/json" \
+--data '{
+  "book": {
+    "title": "'"${TITLE}"'",
+    "author": "'"${AUTHOR}"'"
+  }
+}'
---
+++ scripts/books/books-update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl --include --request PATCH "http://localhost:3000/books/${ID}" \
+--header "Content-Type: application/json" \
+--data '{
+  "book": {
+    "title": "'"${TITLE}"'",
+    "author": "'"${AUTHOR}"'"
+  }
+}'
