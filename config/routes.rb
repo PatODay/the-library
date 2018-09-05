735d7d546346cfb0025bf258697e9948ca34516e
PatODay <poday22@gmail.com>
2018-09-05 16:24:38 -0400

    Scaffolded books resource
    Scaffolded resource with `bin/rails g scaffold book`
    Ran migration with `rails db:migrate`
    Tested CRUD actions successfully

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :books, except: %i[new edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/books' => 'books#index'
+  get '/books/:id' => 'books#show'
+  delete '/books/:id' => 'books#destroy'
+  post '/books' => 'books#create'
+  patch '/books/:id' => 'books#update'
 end
