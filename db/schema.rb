cbe591d4a8ee93e78f4812f14252e6f5f283e628
PatODay <poday22@gmail.com>
2018-09-05 17:13:18 -0400

    Scaffolded resource for authors
    Scaffolded authors resource with `bin/rails g scaffold author first_name:string last_name:string`
    In the migration file added `null: false` to first_name and last_name
    Ran migration with `bin/rails db:migrate`
    In `author` model added the `has_many` reference to `books`
    In the `book` model added the `belongs_to` reference to `author`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_174053) do
+ActiveRecord::Schema.define(version: 2018_09_05_205940) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
