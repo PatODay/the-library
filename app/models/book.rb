c22eb0884554d1ad820fd1e7ead5aa6b424e8ad6
PatODay <poday22@gmail.com>
2018-09-06 00:10:51 -0400

    Created many-to-many relationship via join table
    Added associations to `book` and `borrower` models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :author
+  has_many :borrowers, through: :loans
+  has_many :loans, dependent: :destroy
 end
--- app/models/borrower.rb
+++ app/models/borrower.rb
@@ -1,2 +1,4 @@
 class Borrower < ApplicationRecord
+  has_many :books, through: :loans
+  has_many :loans, dependent: :destroy
 end
