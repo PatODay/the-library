59d6cb6c33d0d94eeef9aa3e4cdfc819d3f3c485
PatODay <poday22@gmail.com>
2018-09-05 23:55:04 -0400

    Modified `books` controllers `book_params`
    Added `:author_id` to permitted params for `book`

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -46,6 +46,6 @@ class BooksController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def book_params
-      params.require(:book).permit(:title, :author)
+      params.require(:book).permit(:title, :author, :author_id)
     end
 end
